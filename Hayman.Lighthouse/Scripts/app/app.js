6169b8d55a6d3c233e9d4ecb1a6f882e947a1dc1
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2011-12-06 13:52:09 +0100

    Added Chosen for Comboboxes and added log4net
    Signed-off-by: Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>

--- Hayman.Lighthouse/Scripts/app/app.js
+++ Hayman.Lighthouse/Scripts/app/app.js
@@ -1,3 +1,6 @@
 ﻿$(function () {
 	$('.close').on('click', function (e) { e.preventDefault(); $(this).parents('.alert-box').slideUp(); });
 });
+
+// Initialize all select boxes as Chosen plugin
+$("select").chosen();
