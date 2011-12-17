54d312c96a84fdf68a023ef53dba4e5fca43bdff
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2011-12-17 18:08:39 +0100

    Reset of Zurb Foundation styles switching to twitter bootstrap

--- Hayman.Lighthouse/Scripts/app/app.js
+++ Hayman.Lighthouse/Scripts/app/app.js
@@ -1,6 +1,1 @@
-﻿$(function () {
-	$('.close').on('click', function (e) { e.preventDefault(); $(this).parents('.alert-box').slideUp(); });
-});
-
-// Initialize all select boxes as Chosen plugin
-$("select").chosen();
+﻿
